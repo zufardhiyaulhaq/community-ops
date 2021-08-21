759de0ccb177430c702faeee2e727883b0f98d1e
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2021-08-21 19:17:13 +0200

    deployer script support multiple clusters
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- scripts/dryrun.py
+++ scripts/dryrun.py
@@ -8,30 +8,37 @@ from deployer.manifest import ManifestDeployer
 from deployer.istio import IstioDeployer
 from utils import *
 
+import click
+
 DEFAULT_CLUSTER_CONFIG = "config.yaml"
 DEFAULT_CLUSTER_MANIFEST_DIR = "manifests/"
 
-def main():
+@click.command()
+@click.option("--cluster-name",
+              help="Cluster name to where the runtime should be deployed to",
+              required=True)
+
+def main(cluster_name):
     configuration = ConfigurationParser(DEFAULT_CLUSTER_CONFIG)
     configuration.validate()
 
     shell = ShellDeployer()
 
     print_header("Dryrun Kustomize")
-    kustomize = KustomizeDeployer(shell)
+    kustomize = KustomizeDeployer(shell, cluster_name)
     kustomize.deploy(dryrun=True)
 
     print_header("Dryrun Manifest")
-    manifest = ManifestDeployer(shell, DEFAULT_CLUSTER_MANIFEST_DIR)
+    manifest = ManifestDeployer(shell, DEFAULT_CLUSTER_MANIFEST_DIR, cluster_name)
     manifest.deploy(dryrun=True)
 
     print_header("Dryrun Helm")
-    helm = HelmDeployer(shell)
+    helm = HelmDeployer(shell, cluster_name)
     helm.deploy(dryrun=True)
 
     print_header("Dryrun Istio")
     istio_config_file = configuration.get_istio()
-    istio = IstioDeployer(shell, istio_config_file)
+    istio = IstioDeployer(shell, istio_config_file, cluster_name)
     istio.deploy(dryrun=True)
 
 if __name__ == '__main__':
