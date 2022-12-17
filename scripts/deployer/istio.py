ab8111945a2d5f9d70f7e3bc07029d35f301ca01
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2022-12-17 16:23:39 +0700

    implement istioctl diff & dryrun
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- scripts/deployer/istio.py
+++ scripts/deployer/istio.py
@@ -4,13 +4,17 @@ class IstioDeployer():
         self.config_file = config_file
         self.cluster = cluster
 
-    # TODO add ability to dryrun the istioctl
-    # https://github.com/istio/istio/pull/34634
     def deploy(self, dryrun=False):
         if dryrun == False:
             command = ["istioctl", "install", "-f", self.config_file, "--force", "--skip-confirmation", "--context", self.cluster]
             self.shell.execute(command)
+        else:
+            command = ["istioctl", "install", "-f", self.config_file, "--force", "--dry-run", "--skip-confirmation", "--context", self.cluster]
+            self.shell.execute(command)
         
-    # TODO add ability to diff the manifest
     def diff(self):
-        return
+        manifest_generate_command = ["istioctl", "manifest", "generate", "-f", self.config_file, ">", "output.yaml"]
+        self.shell.execute(manifest_generate_command)
+
+        diff_command = ["kubectl", "diff", "-f", "output.yaml"]
+        self.shell.execute(diff_command)
